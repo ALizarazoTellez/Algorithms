4e8bfdb81f0c8bf191479daed32ef121e38529ee
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-07-05 21:25:08 -0500

    implement RGB to HSL

--- Languages/Go/pkg/color/color.go
+++ Languages/Go/pkg/color/color.go
@@ -2,6 +2,7 @@ package color
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -13,15 +14,74 @@ func (rgb RGB) String() string {
 	return fmt.Sprintf("rgb(%d, %d, %d)", rgb.R, rgb.G, rgb.B)
 }
 
-func (rgb RGB) ToHex() Hex {
-	return Hex{fmt.Sprintf("%X%X%X", rgb.R, rgb.G, rgb.B)}
+func (rgb RGB) Hex() RGBHex {
+	return RGBHex{fmt.Sprintf("%X%X%X", rgb.R, rgb.G, rgb.B)}
 }
 
-type Hex struct {
+func (rgb RGB) HSL() HSL {
+	r := float32(rgb.R) / 255.0
+	g := float32(rgb.G) / 255.0
+	b := float32(rgb.B) / 255.0
+
+	var cmax float32
+
+	if r > g && r > b {
+		cmax = r
+	} else if g > r && g > b {
+		cmax = g
+	} else {
+		cmax = b
+	}
+
+	var cmin float32
+
+	if r < g && r < b {
+		cmin = r
+	} else if g < r && g < b {
+		cmin = g
+	} else {
+		cmin = b
+	}
+
+	delta := cmax - cmin
+
+	// Get Hue.
+	var hue uint16
+
+	if delta == 0 {
+		hue = 0
+	} else if cmax == r {
+		hue = uint16(60*math.Mod(float64((g-b)/delta), 6) + 0.5)
+	} else if cmax == g {
+		hue = uint16(60*(((b-r)/delta)+2) + 0.5)
+	} else if cmax == b {
+		hue = uint16(60*(((r-g)/delta)+4) + 0.5)
+	}
+
+	// Get Lightness.
+	lightness := (cmax + cmin) / 2
+
+	// Get Saturation.
+	var sat float32
+
+	if delta == 0 {
+		sat = 0
+	} else {
+		sat = delta / float32(1-math.Abs(float64(2*lightness-1)))
+	}
+
+	return HSL{hue, sat * 100, lightness * 100}.Normalize()
+}
+
+type RGBHex struct {
 	Hex string
 }
 
-func (h Hex) ToRGB() RGB {
+func (h RGBHex) String() string {
+	return "#" + h.Hex
+}
+
+func (h RGBHex) RGB() RGB {
 	var rgb RGB
 
 	r, _ := strconv.ParseUint(h.Hex[0:2], 16, 8)
@@ -34,4 +94,26 @@ func (h Hex) ToRGB() RGB {
 	rgb.B = uint8(b)
 
 	return rgb
+}
+
+type HSL struct {
+	H uint16
+	S float32
+	L float32
+}
+
+func (hsl HSL) String() string {
+	return fmt.Sprintf("hsl(%d, %.1f, %.1f)", hsl.H, hsl.S, hsl.L)
+}
+
+func (hsl HSL) Normalize() HSL {
+	return HSL{
+		hsl.H,
+		float32(math.Round(float64(hsl.S*10)) / 10),
+		float32(math.Round(float64(hsl.L*10)) / 10),
+	}
+}
+
+func (hsl HSL) RGB() RGB {
+	return RGB{}
 }
--- Languages/Go/pkg/color/color_test.go
+++ Languages/Go/pkg/color/color_test.go
@@ -8,26 +8,34 @@ import (
 func TestColorConvert(t *testing.T) {
 	tests := []struct {
 		rgb RGB
-		hex Hex
+		hex RGBHex
+		hsl HSL
 	}{
-		{RGB{26, 93, 26}, Hex{"1A5D1A"}},
-		{RGB{241, 201, 59}, Hex{"F1C93B"}},
-		{RGB{251, 216, 93}, Hex{"FBD85D"}},
-		{RGB{250, 227, 146}, Hex{"FAE392"}},
+		{RGB{26, 93, 26}, RGBHex{"1A5D1A"}, HSL{120, 56.3, 23.3}},
+		{RGB{241, 201, 59}, RGBHex{"F1C93B"}, HSL{47, 86.7, 58.8}},
+		{RGB{251, 216, 93}, RGBHex{"FBD85D"}, HSL{47, 95.2, 67.5}},
+		{RGB{250, 227, 146}, RGBHex{"FAE392"}, HSL{47, 91.2, 77.6}},
 	}
 
 	for _, tt := range tests {
-		t.Run(fmt.Sprintf("convert %v to %s", tt.rgb, tt.hex), func(t *testing.T) {
-			got := tt.rgb.ToHex()
+		t.Run(fmt.Sprintf("convert %q to %q", tt.rgb, tt.hex), func(t *testing.T) {
+			got := tt.rgb.Hex()
 			if got != tt.hex {
 				t.Errorf("want %q, got %q", tt.hex, got)
 			}
 		})
 
-		t.Run(fmt.Sprintf("convert %s to %v", tt.hex, tt.rgb), func(t *testing.T) {
-			got := tt.hex.ToRGB()
+		t.Run(fmt.Sprintf("convert %q to %q", tt.hex, tt.rgb), func(t *testing.T) {
+			got := tt.hex.RGB()
 			if got != tt.rgb {
 				t.Errorf("want %q, got %q", tt.rgb, got)
+			}
+		})
+
+		t.Run(fmt.Sprintf("convert %q to %q", tt.rgb, tt.hsl), func(t *testing.T) {
+			got := tt.rgb.HSL()
+			if got != tt.hsl {
+				t.Errorf("want %q, got %q", tt.hsl, got)
 			}
 		})
 	}
